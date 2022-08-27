614d3683381b813c894cb64e1d2abbf8f9530b62
Akinsuyiphilip <philipakinbolade@gmail.com>
2022-08-27 23:09:46 +0100

    file add

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 const textarea = document.querySelector("textarea"),
-FileNameInput = document.querySelector(".file-name input"),
-selectMenu = document.querySelector(".save-as select"),
-saveBtn = document.querySelector(".save-btn");
+    FileNameInput = document.querySelector(".file-name input"),
+    selectMenu = document.querySelector(".save-as select"),
+    saveBtn = document.querySelector(".save-btn");
 
-saveBtn.addEventListener('click', () =>{
-    console.log(textarea.value)
-})
+
+window.onload = () => {
+
+    // To make texarea cleared on load
+    textarea.value = "";
+
+
+
+
+    let selectedOption = selectMenu.options[selectMenu.selectedIndex].text;
+
+    saveBtn.innerText = `Save As ${selectedOption.split(" ")[0]}`
+
+
+
+    saveBtn.addEventListener('click', () => {
+        const blob = new Blob([textarea.value], {
+            type: selectMenu.value
+        });
+        // Url.createObjectURL create a url that represent a passed object
+        const fileUrl = URL.createObjectURL(blob);
+        const Link = document.createElement("a"); // creating a tag
+        Link.download = FileNameInput.value; //passing file name download value of link
+        Link.href = fileUrl; // passing fileurl  as href of link
+        Link.click();
+    })
+
+
+}
